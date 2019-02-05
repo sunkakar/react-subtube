748e92625d523351919747153e91d8b5da221849
Sundeep Kakar <skakar@iu.edu>
2019-02-05 15:07:56 -0500

    Updates

---
+++ precache-manifest.c5af478fcdbd6575df65c74197209c08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ab0ed9beb6ec2ca888",
+    "url": "/react-subtube/static/js/runtime~main.42ab0ed9.js"
+  },
+  {
+    "revision": "589627dcca821bf7c6bd",
+    "url": "/react-subtube/static/js/main.589627dc.chunk.js"
+  },
+  {
+    "revision": "3804d5785cb97c312664",
+    "url": "/react-subtube/static/js/1.3804d578.chunk.js"
+  },
+  {
+    "revision": "589627dcca821bf7c6bd",
+    "url": "/react-subtube/static/css/main.5c987d8b.chunk.css"
+  },
+  {
+    "revision": "288091f6e3cf4e9644e7ccdfe7595cd6",
+    "url": "/react-subtube/index.html"
+  }
+];
