90606b26e357071425fee2b350ee0381849f406b
sunkakar <sundeepkakar@gmail.com>
2019-06-12 14:33:00 -0400

    Added Firebase Hosting

---
+++ build/precache-manifest.fba6da508276fd09daadcf1c9e0df78f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d3413be0f0f469df0aa",
+    "url": "/static/js/main.3d3413be.chunk.js"
+  },
+  {
+    "revision": "f77c6a10013f0fa73ffd",
+    "url": "/static/js/1.f77c6a10.chunk.js"
+  },
+  {
+    "revision": "3d3413be0f0f469df0aa",
+    "url": "/static/css/main.a3620a29.chunk.css"
+  },
+  {
+    "revision": "f46dd163cb6a1d885a0226ad88c64380",
+    "url": "/index.html"
+  }
+];
--- src/components/google-auth.js
+++ src/components/google-auth.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import GoogleLogin from 'react-google-login';
+//import GoogleLogin from 'react-google-login';
 // or
 //import { GoogleLogin } from 'react-google-login';
  
  
-const responseGoogle = (response) => 
-{
-  console.log(response);
-}
+// const responseGoogle = (response) => 
+// {
+//   console.log(response);
+// }
 
 class GoogleAuth extends React.Component
 {
@@ -15,12 +15,13 @@ class GoogleAuth extends React.Component
   render()
   {
     return(
-      <GoogleLogin
-      clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-      buttonText="Login"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-      />
+      // <GoogleLogin
+      // clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+      // buttonText="Login"
+      // onSuccess={responseGoogle}
+      // onFailure={responseGoogle}
+      // />
+      <h1>Hello</h1>
     );
   }
 }  
